f289554f3f5a3796ac2d755721821e5043518975
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-28 22:55:17 +0600

    ui almost done

--- js/phone.js
+++ js/phone.js
@@ -4,6 +4,38 @@ const loadPhone = async () => {
   );
   const data = await res.json();
   const phones = data.data;
-  console.log(phones);
+  //   console.log(phones);
+  displayPhone(phones);
 };
+
+const displayPhone = (phones) => {
+  //   console.log(phones);
+
+  const phoneContainer = document.getElementById("phone-container");
+
+  phones.forEach((phone) => {
+    console.log(phone);
+    // create phone container div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList =
+      "card my-8 mx-4 py-8 text-center bg-gray-100 shadow-xl";
+    phoneCard.innerHTML = `
+        <figure>
+            <img
+            src=${phone.image}
+            alt=${phone.slug}
+            />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title mx-auto">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-center">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+    `;
+    phoneContainer.appendChild(phoneCard);
+  });
+};
+
 loadPhone();
