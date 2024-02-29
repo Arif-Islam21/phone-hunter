47afd7d3f66dfb37e2b0baf6de2bf267a84fc3ef
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-29 10:43:27 +0600

    dsljfgrdhjfs

--- js/mine.js
+++ js/mine.js
@@ -2,16 +2,48 @@ const loadPhone = async (phoneText) => {
   const response = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${phoneText}`
   );
-  const data = response.json();
-  console.log(data);
+  const data = await response.json();
+  const phoneData = data.data;
+
+  displayPhones(phoneData);
 };
 
 const inputElement = document.getElementById("search-field");
 
 document.getElementById("search-btn").addEventListener("click", function () {
   const inputText = inputElement.value;
-  loadPhone(phoneText);
+  loadPhone(inputText);
   //   console.log(inputText);
 });
 
-loadPhone();
+const displayPhones = (phones) => {
+  if (phones.length > 12) {
+    const showAllBtn = document.getElementById("showAllContainer");
+    showAllBtn.classList.remove("hidden");
+    showAllBtn.classList.add("block");
+    console.log(showAllBtn);
+  }
+
+  const phoneContainer = document.getElementById("phone-container");
+  phoneContainer.textContent = "";
+
+  //   more then 12
+  phones = phones.slice(0, 5);
+
+  for (const phone of phones) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+  <div class="card  bg-gray-100 my-4 gap-4 py-4 shadow-xl">
+  <figure><img src=${phone.image} alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">${phone.phone_name}</h2>
+    <p>${phone.slug}</p>
+    <div class="card-actions justify-center">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+  `;
+    phoneContainer.appendChild(div);
+  }
+};
